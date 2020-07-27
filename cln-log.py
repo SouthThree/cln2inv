7a5eee6f100530ccdf3f960805c065b5911874a0
SouthThree <1358270705@qq.com>
2020-07-27 19:02:31 +0800

    init the project and add "cln-log.py" to run tests

---
+++ cln-log.py
@@ -0,0 +1,9 @@
+import os
+if os.path.exists("result"):
+    os.removedirs("result")
+os.mkdir("result")
+for i in range(1, 134):
+    print("running %d th tests" % i)
+    #print('python3 cln2inv.py ' + str(i) + '> ' + str(i) + '.txt')
+    os.system('python3 cln2inv.py ' + str(i) + ' > ./result/' + str(i) + '.txt')
+print("Done!")
